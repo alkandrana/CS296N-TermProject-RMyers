5ff14d8dcf5fde85f1b5ffa6925d93718473a17a
Rosa Myers <larosaardente@gmail.com>
2025-03-03 16:20:50 -0800

    Initial commit.

---
+++ CS296N-TermProject-RMyers/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CS296N_TermProject_RMyers.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
